61addc2899f29310dc44808b38b93eb4458b222f
Eduardo Henrique Gris <gris.eduardo@gmail.com>
2020-08-07 10:36:44 -0300

    Visitante abre o site

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  #babysteps
+  def index
+  end
+end
---
+++ spec/features/visitor_open_home_page_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'Visitor open home page' do
+  scenario 'successfully' do
+    #3 As -> Arrange, Act, Assert
+    #3 As -> Prepara, Executa, Valida
+    # Arrange
+    # Act
+    visit root_path
+
+    #Assert
+    expect(page).to have_css('h1', text: 'Rental Cars')
+    expect(page).to have_content('Bem vindo ao sistema de gestão de locação')
+  end
+end
