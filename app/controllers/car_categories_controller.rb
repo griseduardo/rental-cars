ec5e42640b8d02c6cfe8e8d9b839cb0ffaaad287
Eduardo Henrique Gris <gris.eduardo@gmail.com>
2020-08-11 11:21:31 -0300

    Registro de categorias de carro

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -6,4 +6,20 @@ class CarCategoriesController < ApplicationController
   def show
     @car_category = CarCategory.find(params[:id]) #params é um HASH    
   end
+
+  def new
+    @car_category = CarCategory.new    
+  end
+
+  def create
+    @car_category = CarCategory.create(car_category_params)
+    redirect_to @car_category 
+  end
+
+  private
+
+  def car_category_params
+    params.require(:car_category)
+          .permit(:name, :daily_rate, :car_insurance, :third_party_insurance)
+  end
 end
