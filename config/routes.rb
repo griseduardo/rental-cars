2038bd0bad1802e5bfa2fd3f152d48d782747537
Eduardo Henrique Gris <gris.eduardo@gmail.com>
2020-08-13 16:18:36 -0300

    Apagando categorias de carro

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :car_categories, only: [:index, :show, :new, :create, :edit, :update]
-  resources :subsidiaries, only: [:index, :show, :new, :create, :edit, :update]
+  resources :car_categories
+  resources :subsidiaries
 end
