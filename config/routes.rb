169fa433d05ddc1c2a6407fbb264c84e25df3252
Eduardo Henrique Gris <gris.eduardo@gmail.com>
2020-08-13 15:27:24 -0300

    WIP: Editar categoria de carro

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :car_categories, only: [:index, :show, :new, :create]
-  resources :subsidiaries, only: [:index, :show, :new, :create]
+  resources :car_categories, only: [:index, :show, :new, :create, :edit, :update]
+  resources :subsidiaries, only: [:index, :show, :new, :create, :edit, :update]
 end
